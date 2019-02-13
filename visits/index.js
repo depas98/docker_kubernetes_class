78fe20300ddd66f3a4d4ee9d49c8a42146f4c3a0
depas <depas98@hotmail.com>
2019-02-12 22:19:21 -0700

    Fixed visits example

--- visits/index.js
+++ visits/index.js
@@ -4,9 +4,13 @@ const redis = require('redis');
 const app = express();
 // had to change because docker toolbox runs on 192.168.99.100 not localhost
 // const client = redis.createClient(); 
-const client = redis.createClient({host: "192.168.99.100", port: 6379});    
+// const client = redis.createClient({host: "192.168.99.100", port: 6379});    
+// this works because of docker compose
+const client = redis.createClient({
+    host: 'redis-server', 
+    port: 6379});
 
-//  initialize teh visits value on the redis server
+//  initialize the visits value on the redis server
 client.set('visits', 0);
 
 app.get("/", (req, res) => {
@@ -19,4 +23,3 @@ app.get("/", (req, res) => {
 app.listen(8081, () => {
     console.log('Listening on port 8081');
 });
-
